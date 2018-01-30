de9c6e63a7bb76ffa0527b9d8c661e6f7d0bbd05
Mohit Savaliya <mohitsavaliya111@gmail.com>
2018-01-30 13:25:40 +0530

    Update feefteen.js

--- feefteen.js
+++ feefteen.js
@@ -2,7 +2,7 @@ let gmail = function(str){
 	let arr = str.split(" ");
 	let ans = [];
   	for(let x of arr){	
-  		if(x.match(/@gmail.com/g)!=null)
+  		if(x.match(/@.*?\.com/g)!=null)
   		ans.push(x);
 	}
 	return ans;
